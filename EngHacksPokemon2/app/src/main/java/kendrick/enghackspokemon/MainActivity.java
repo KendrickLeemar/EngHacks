e9a209718b75fbf3a17ffbab1eba5f3bd0c98534
KendrickLeemar <ken132132@gmail.com>
2016-10-28 20:45:42 -0400

    Finished the very basics

--- EngHacksPokemon2/app/src/main/java/kendrick/enghackspokemon/MainActivity.java
+++ EngHacksPokemon2/app/src/main/java/kendrick/enghackspokemon/MainActivity.java
@@ -8,38 +8,43 @@ import android.widget.*;
 public class MainActivity extends AppCompatActivity {
     private Button mAskType;
     private TextView mPromptText;
-
+    private EditText mTypeField;
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAskType = (Button) findViewById(R.id.askType);
         mPromptText = (TextView) findViewById(R.id.promptText);
-
+        mTypeField = (EditText) findViewById(R.id.editText);
+        mTextView = (TextView) findViewById(R.id.textView);
         mAskType.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
+                String type =  mTypeField.getText().toString();
+                mTextView.setText(processInformation(type));
+            }
+        });
 
-                                        }
-                                    }
-        );
     }
-
 
 
     private String processInformation(String type) {
         if (type.equalsIgnoreCase("fire")) {
             return "Weak Against Water, Ground, and Rock.\n" +
                     "Resists Grass, Fairy, Bug, Ice, and Steel";
-        }
-        if (type.equalsIgnoreCase("water")) {
+        } else if (type.equalsIgnoreCase("water")) {
             return "Weak Against Grass, and Electric" +
                     "Resists Fire, Water, Ice and Steel";
-
+        } else if (type.equalsIgnoreCase("grass")) {
+            return "Weak Against Fire, Bug, Flying, and Poison" +
+                    "Resists Grass, Ground";
+        } else if (type.equalsIgnoreCase("electric")){
+            return "Weak Against Ground" +
+                    "Resists Electric, Flying and Steel";
+        } else if (type.equalsIgnoreCase("ice")){
+            return "Weak Against Fighting, Fire, Rock, Steel ";
         }
-        if (type.equalsIgnoreCase("grass")){
-            return "Weak Against Fire, Bug, Flying and Poison" +
-                    "Resists Grass, Ground"
-        }
+        return "Enter a type dingus";
     }
 }
